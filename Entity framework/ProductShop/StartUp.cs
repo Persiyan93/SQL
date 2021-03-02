be0094bbb76be8fa0e4b17e4987610f15519086f
Persiyan93 <persiyan.georgiev@gmail.com>
2021-03-02 18:20:21 +0200

    Initial migration

---
+++ Entity framework/ProductShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            Console.WriteLine(ImportUsers(context, @"D:\Work\SQL\SQL\Entity framework\ProductShop\Datasets\users.json"));
+         
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(inputJson));
+            foreach (var user in users)
+            {
+                context.Users.Add(user);
+            }
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+
+    }
+}

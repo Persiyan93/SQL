f59cdf96a8f8d6b40f5d81e468897cb5a25d37f1
Persiyan93 <persiyan.georgiev@gmail.com>
2021-03-08 19:46:08 +0200

    Add methods which read from xml files and import them in database

--- Entity framework/CarDeale/CarDealer/Models/Supplier.cs
+++ Entity framework/CarDeale/CarDealer/Models/Supplier.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace CarDealer.Models
 {
+    [XmlType("Supplier")]
     public class Supplier
     {
         public int Id { get; set; }
 
+        [XmlElement(ElementName ="name")]
         public string Name { get; set; }
-
+        [XmlElement(ElementName = "isImporter")]
         public bool IsImporter { get; set; }
 
+        [XmlIgnore]
         public ICollection<Part> Parts { get; set; }
     }
 }
--- Entity framework/CarDeale/CarDealer/StartUp.cs
+++ Entity framework/CarDeale/CarDealer/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using CarDealer.Data;
@@ -16,89 +17,26 @@ namespace CarDealer
         public static void Main(string[] args)
         {
             var context = new CarDealerContext();
-            var json = File.ReadAllText(@"D:\Work\SQL\SQL\Entity framework\CarDeale\CarDealer\Datasets\cars.json");
-            Console.WriteLine(ImportCars(context, json));
+            var xml = File.ReadAllText(@"D:\Work\SQL\SQL\Entity framework\CarDeale\CarDealer\Datasets\suppliers.xml");
+            Console.WriteLine(xml);
+            Console.WriteLine(ImportSuppliers(context, xml));
         }
-        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        public static string ImportSuppliers(CarDealerContext context, string inputxml)
         {
-            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+            var serializer = new XmlSerializer(typeof(Supplier[]), new XmlRootAttribute("Suppliers"));
+
+            var suppliers = (Supplier[])serializer.Deserialize(new StringReader(inputxml));
+            
             context.Suppliers.AddRange(suppliers);
             context.SaveChanges();
 
-            return $"Successfully imported {suppliers.Count}.";
-        }
-        public static string ImportParts(CarDealerContext context, string inputJson)
-        {
-            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
-            var supplierIds = context.Suppliers.Select(s => s.Id).ToList();
-            parts = parts.Where(p => supplierIds.Contains(p.SupplierId) == true).ToList();
-            context.Parts.AddRange(parts);
-            context.SaveChanges();
-            return $"Successfully imported {parts.Count}.";
-
-        }
-
-        public static string ImportCars(CarDealerContext context, string inputJson)
-        {
-            var config = new MapperConfiguration(cfg =>
-                  cfg.AddProfile<CarDealerProfile>());
-            var mapper = config.CreateMapper();
-
-            var inputCars = JsonConvert.DeserializeObject<List<CarInputModel>>(inputJson);
-            foreach (var car in inputCars)
-            {
-                var dbCar = mapper.Map<Car>(car);
-                context.Add(dbCar);
-                context.SaveChanges();
-                var carParts = new List<PartCar>();
-                foreach (var partId in car.PartsId)
-                {
-                    var partCar = new PartCar { PartId = partId, CarId = dbCar.Id };
-                    carParts.Add(partCar);
-                }
-                dbCar.PartCars = carParts;
-                context.SaveChanges();
-
-
-
-            }
-           
-
-
-
-
-
-            // var carTest = mapper.Map<ICollection<Car>>(inputCars);
-
-            // context.AddRange(carTest);
-            //context.SaveChanges();
-            
-
-            //var cars = inputCars.Select(c => new Car
-            //{
-            //    Make = c.Make,
-            //    Model = c.Model,
-            //    TravelledDistance = c.TravelledDistance,
-            //    PartCars = c.PartsId.Select(x => new PartCar { PartId = x, }).ToList()
-
-
-            //});
-
-
-            //context.Cars.AddRange(inputCars.Select(c => new Car
-            //{
-            //    Make = c.Make,
-            //    TravelledDistance = c.TravelledDistance,
-            //    PartCars = c.PartsId.Select(x => new PartCar { PartId = x, }).ToList()
-
-
-            //}).ToList());
-
-            return null;
-            //return $"Successfully imported {cars.Count()}.";
+            return $"Successfully imported {suppliers.Length}.";
         }
 
 
 
-}
+
+
+
+    }
 }

da89fc7a034dfa9f3d7e93fb8b82ed811ad64b5b
Persiyan93 <persiyan.georgiev@gmail.com>
2021-03-08 20:53:22 +0200

    Add method which remove duplicate parts from cars.xml

--- Entity framework/CarDeale/CarDealer/Models/Part.cs
+++ Entity framework/CarDeale/CarDealer/Models/Part.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace CarDealer.Models
 {
     public class Part
     {
+
         public int Id { get; set; }
 
+        [XmlElement(ElementName = "name")]
         public string Name { get; set; }
-
+        [XmlElement(ElementName = "price")]
         public decimal Price { get; set; }
-
+        [XmlElement(ElementName = "quantity")]
         public int Quantity { get; set; }
-
+        [XmlElement(ElementName = "supplierId")]
         public int SupplierId { get; set; }
-        public Supplier Supplier { get; set; }
 
+
+        public Supplier Supplier { get; set; }
+        [XmlIgnore]
         public ICollection<PartCar> PartCars { get; set; }
     }
 }
--- Entity framework/CarDeale/CarDealer/StartUp.cs
+++ Entity framework/CarDeale/CarDealer/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -16,21 +17,70 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
-            var context = new CarDealerContext();
-            var xml = File.ReadAllText(@"D:\Work\SQL\SQL\Entity framework\CarDeale\CarDealer\Datasets\suppliers.xml");
-            Console.WriteLine(xml);
-            Console.WriteLine(ImportSuppliers(context, xml));
+            RemoveDublicateParts();
+
+
+            //var context = new CarDealerContext();
+            //var xml = File.ReadAllText(@"D:\Work\SQL\SQL\Entity framework\CarDeale\CarDealer\Datasets\cars.xml");
+
+            //Console.WriteLine(ImportCars(context, xml));
         }
+
+        private static void RemoveDublicateParts()
+        {
+            var xmldoc = XDocument.Load(@"D:\Work\SQL\SQL\Entity framework\CarDeale\CarDealer\Datasets\cars.xml");
+            var cars = xmldoc.Root.Elements();
+            foreach (var car in cars)
+            {
+                var parts = car.Element("parts").Elements("partId");
+                var existingParts = new List<int>();
+                foreach (var part in parts)
+                {
+                    var currpart = part.Attribute("id").Value;
+                    if (existingParts.Contains(int.Parse(currpart)))
+                    {
+                        part.Remove();
+                    }
+                    existingParts.Add(int.Parse(currpart));
+                }
+
+              
+              
+
+            }
+            Console.WriteLine(xmldoc);
+            xmldoc.Save("cars.xml");
+        }
+
         public static string ImportSuppliers(CarDealerContext context, string inputxml)
         {
             var serializer = new XmlSerializer(typeof(Supplier[]), new XmlRootAttribute("Suppliers"));
-
             var suppliers = (Supplier[])serializer.Deserialize(new StringReader(inputxml));
-            
+
             context.Suppliers.AddRange(suppliers);
             context.SaveChanges();
 
             return $"Successfully imported {suppliers.Length}.";
+        }
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+
+            var serializer = new XmlSerializer(typeof(Part[]), new XmlRootAttribute("Parts"));
+            var parts = (Part[])serializer.Deserialize(new StringReader(inputXml));
+            var supplierIds = context.Suppliers.Select(s => s.Id).ToList();
+            parts = parts.Where(p => supplierIds.Any(x => x == p.SupplierId)).ToArray();
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Length}.";
+
+
+        }
+
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            return null;
         }
 
 
